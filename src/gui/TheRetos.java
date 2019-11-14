010b811acc3707c68a56f9b1658e04e59d36dbde
Oscar <o.guzman@udla.edu.co>
2019-11-13 23:23:26 -0500

    al cerrar programa cierra conexion con arduino y apaga los leds

--- src/gui/TheRetos.java
+++ src/gui/TheRetos.java
@@ -1,12 +1,29 @@
 package gui;
+import arduino.Arduino;
+
 import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 public class TheRetos extends JFrame {
     private TheRetos(){
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setContentPane(new Contenedor());
-        pack();
+        pack();//ajusta todo el contenido//
         setLocationRelativeTo(null);
         setResizable(false);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                Arduino arduino = Arduino.getConexion();
+                arduino.ledOffAll();
+                arduino.closeOnConexion();
+            }
+            @Override
+            public void windowClosed(WindowEvent e) {
+                System.exit(0);
+            }
+        });
     }
     public static void main(String[] args) {
         new TheRetos().setVisible(true);
